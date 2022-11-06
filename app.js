07524c8543c5c404c0016c6bca2eae1fb14c8da5
Christopher Shaw <silvertn350@yahoo.com>
2022-11-06 10:04:54 -0600

    1st commit.

---
+++ app.js
@@ -0,0 +1,5 @@
+var message = 'Hello, TypeScript !';
+console.log(message);
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello, TypeScript !';
+console.log(message);
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+document.body.appendChild(heading);
